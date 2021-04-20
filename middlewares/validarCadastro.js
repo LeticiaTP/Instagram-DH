2a47770d66f7c16a002d136cd756ee0a0e38bd9a
Letícia Paulino <karlaleticiatavares@gmail.com>
2021-04-20 13:58:16 -0300

    conserta erro em validarCadastro.js

--- middlewares/validarCadastro.js
+++ middlewares/validarCadastro.js
@@ -1,13 +1,29 @@
-const { Usuario } = require('../models')
-​
+const { Usuario } = require('../models');
+
 module.exports = async (request, response, next) => {
-    let { email } = request.body;
-    let users = await Usuario.findAll({ where: { email } });
-    if (users.length) {
-        response.status(400).json({ erro: "Email já cadastrado" })
+    let { nome, email, senha } = request.body;
+    let usuarios = await Usuario.findAll({ where: { email } });
+
+    if(nome && email && senha){
+
+        if (!usuarios.length) {
+
+            if(senha.length >= 6 && senha.length <= 12){
+                next();
+
+            } else {
+                response.status(400).json({ erro: "A senha deve ter entre 6 e 12 caracteres." });
+                return;
+                }  
+
+        } else {
+        response.status(400).json({ erro: "Email já cadastrado." });
         return;
+        }
+
     } else {
-        next();
+        response.status(400).json({ erro: "Por favor, preencha todos os campos!" })
+        return;
     }
-​
+
 }
